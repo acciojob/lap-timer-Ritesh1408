20c686905237e77e39ca2ab91fdbdda240e990ba
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-05-06 18:38:06 +0000

    Update the codebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,68 @@
+import React, { useState, useRef, useEffect } from "react";
 
-import React from "react";
-import './../styles/App.css';
+function App() {
+  const [time, setTime] = useState(0);
+  const [laps, setLaps] = useState([]);
+  const intervalRef = useRef();
+  const handleStart = () => {
+    if (!intervalRef.current) {
+      intervalRef.current = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    }
+  };
 
-const App = () => {
+  const handleStop = () => {
+    clearInterval(intervalRef.current);
+    intervalRef.current = false;
+  };
+  
+  const handleLap = () => {
+    setLaps((prv)=>[...prv,time]);
+  };
+
+  const handleReset = () => {
+    clearInterval(intervalRef.current);
+    intervalRef.current = false;
+    setTime(0);
+    setLaps([]);
+  };
+
+  useEffect(() => {
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <div>{formatTime(time)}</div>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+      <button onClick={handleLap}>Lap</button>
+      <button onClick={handleReset}>Reset</button>
+      <ul>
+        {laps.map((lapTime, index) => (
+          <li key={index}>{formatTime(lapTime)}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+function formatTime(time) {
+  const minutes = Math.floor(time / 60000);
+  const seconds = Math.floor((time - minutes * 60000) / 1000);
+  const centiseconds = Math.floor(
+    (time - minutes * 60000 - seconds * 1000) / 10
+  );
+
+  return `${padNumber(minutes)}:${padNumber(seconds)}:${padNumber(
+    centiseconds
+  )}`;
+}
+
+function padNumber(number) {
+  return number.toString().padStart(2, "0");
+}
+export default App; 
